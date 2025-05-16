2f30dff07389b105dc0a76ac35cc40edd858fade
Josue Mullo <josue.mullo@udla.edu.ec>
2025-05-15 20:20:31 -0500

    Creacion de Modelo GeminiRequest

---
+++ TallerConexionGeminiChaGPTGrupo4/Models/GeminiRequest.cs
@@ -0,0 +1,21 @@
+﻿namespace TallerConexionGeminiChaGPTGrupo4.Models
+{
+    public class GeminiRequest
+    {
+        public class Root
+        {
+            public List<Content> contents { get; set; }
+        }
+
+        public class Content
+        {
+            public List<Part> parts { get; set; }
+        }
+
+        public class Part
+        {
+            public string text { get; set; }
+
+        }
+    }
+}
