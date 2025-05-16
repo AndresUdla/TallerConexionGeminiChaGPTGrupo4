1b5d08b11fc3df614d9b818cc35c112c8756be40
Josue Mullo <josue.mullo@udla.edu.ec>
2025-05-15 20:57:08 -0500

    Creacion de GeminiRepository para controlar la logica, se ha implementado la interfaz de momento

---
+++ TallerConexionGeminiChaGPTGrupo4/Repositories/GeminiRepository.cs
@@ -0,0 +1,12 @@
+﻿using TallerConexionGeminiChaGPTGrupo4.Interfaces;
+
+namespace TallerConexionGeminiChaGPTGrupo4.Repositories
+{
+    public class GeminiRepository : IChatBotService
+    {
+        public string GetChatBotResponse(string prompt)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
