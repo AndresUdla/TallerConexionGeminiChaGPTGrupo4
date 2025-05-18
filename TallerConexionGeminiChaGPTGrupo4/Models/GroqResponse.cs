e2f47cd11b45696e0ca2e1fa9173cd01c823e866
Josue Mullo <josue.mullo@udla.edu.ec>
2025-05-18 14:59:19 -0500

    Creacion de modelo GroqResponse para recibir respuesta de Groq

---
+++ TallerConexionGeminiChaGPTGrupo4/Models/GroqResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace TallerConexionGeminiChaGPTGrupo4.Models
+{
+    public class GroqResponse
+    {
+        public List<Choice> choices { get; set; }
+    }
+
+    public class Choice
+    {
+        public Message message { get; set; }
+    }
+}
